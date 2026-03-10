4fd0126fef1a8e811d44f6769a3935226c4aaa96
Mariana909 <karla.duque01@usa.edu.co>
2026-03-10 09:48:11 -0500

    Create mcd.hs

---
+++ Punto_4/mcd.hs
@@ -0,0 +1,25 @@
+import System.CPUTime
+import Text.Printf
+
+mcd :: Int -> Int -> Int
+mcd a 0 = a
+mcd a b = mcd b (mod a b)
+
+timeFunc :: IO a -> IO a
+timeFunc action = do
+    start  <- getCPUTime
+    result <- action
+    end    <- getCPUTime
+    let diff = fromIntegral (end - start) / (10^12) :: Double
+    printf "Tiempo de ejecución: %0.6f sec\n" diff
+    return result
+
+main :: IO ()
+main = do
+    linea <- getLine
+    let [a, b] = map read (words linea) :: [Int]
+    resultado <- timeFunc $ do
+        let r = mcd a b
+        print r        -- fuerza evaluación lazy
+        return r
+    putStrLn $ "MCD(" ++ show a ++ ", " ++ show b ++ ") = " ++ show resultado
